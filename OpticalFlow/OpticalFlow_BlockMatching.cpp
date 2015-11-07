6b02d69b7496ae0ca7d2d4093c8fec9e495805b3
sh-konta <kon.bass@gmail.com>
2015-11-08 01:05:19 +0900

    Change Block Matching function according to ImgClass
    ImgClass::BlockMatching changed the definition of Motion Vector as generally used; vector from the next frame points the point on previous frame.
    So OpticalFlow_BlockMatching should use BlockMatching::block_matching_forward() for it behaving as it used to.

--- OpticalFlow/OpticalFlow_BlockMatching.cpp
+++ OpticalFlow/OpticalFlow_BlockMatching.cpp
@@ -69,7 +69,8 @@ OpticalFlow_BlockMatching(const ImgVector<double>* It, const ImgVector<double>* 
 
 	// ----- Block Matching -----
 	block_matching.reset(It, Itp1, MotionParam.BlockMatching_BlockSize);
-	block_matching.block_matching(BM_Search_Range);
+	//block_matching.block_matching(BM_Search_Range);
+	block_matching.block_matching_forward(BM_Search_Range);
 	Motion_Vector.copy(block_matching.data());
 
 	// ----- Optical Flow -----
